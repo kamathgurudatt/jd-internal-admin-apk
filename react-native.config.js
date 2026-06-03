78d87751a393caa7ebdcf31fb1fa0a86fe211453
Gurudatt Kamath <gurudatt.kamath@justdial.com>
2026-06-03 15:11:34 +0530

    DIAGNOSTIC: disable all native module autolinking via react-native.config.js

--- react-native.config.js
+++ react-native.config.js
@@ -5,4 +5,15 @@ module.exports = {
       sourceDir: './android',
     },
   },
+  // DIAGNOSTIC: disable all native module autolinking so CMake doesn't fail
+  // on missing JNI directories. Remove these exclusions when adding modules back.
+  dependencies: {
+    'react-native-screens':              { platforms: { android: null } },
+    'react-native-safe-area-context':    { platforms: { android: null } },
+    'react-native-gesture-handler':      { platforms: { android: null } },
+    'react-native-encrypted-storage':    { platforms: { android: null } },
+    '@react-navigation/native':          { platforms: { android: null } },
+    '@react-navigation/bottom-tabs':     { platforms: { android: null } },
+    '@react-navigation/stack':           { platforms: { android: null } },
+  },
 };
